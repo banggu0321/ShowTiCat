c8fc03192253c3644816fae2c397b5f1138fdf7a
HongJayoung <ghddlwkdud@naver.com>
2022-06-07 10:17:03 +0900

    commit_22.06.07

---
+++ src/main/java/com/kos/showticat/util/DBUtil.java
@@ -0,0 +1,40 @@
+package com.kos.showticat.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBUtil {
+
+	public static Connection getConnection() {
+		
+		String url = "jdbc:oracle:thin:@testdb_medium?TNS_ADMIN=C:/wallet_db/Wallet_testDB/";
+		String user="admin";
+		String password ="Kostatestdb241";
+		
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			conn = DriverManager.getConnection(url, user, password);			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	public static void dbClose(ResultSet rs, Statement st, Connection conn) {
+		
+		try {
+			if(rs != null) rs.close();
+			if(st != null) st.close();
+			if(conn != null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
