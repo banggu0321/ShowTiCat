d6fecf0bb4214bf41fadd29d39a2a4781b966af0
jaeyong <lkmdl@gmail.com>
2022-06-10 16:17:38 +0900

    .

--- src/main/java/com/kos/showticat/jayoung/MemberService.java
+++ src/main/java/com/kos/showticat/jayoung/MemberService.java
@@ -2,8 +2,6 @@ package com.kos.showticat.jayoung;
 
 import java.util.List;
 
-import com.kos.showticat.jayoung.MemberVO;
-
 public class MemberService {
 	MemberDAO dao = new MemberDAO();
 
@@ -35,31 +33,6 @@ public class MemberService {
 	// 회원가입
 	public int memberInsert(MemberVO member) {
 		return dao.memberInsert(member);
-	}
-
-	// 개인정보 수정
-	public int memberUpdate(MemberVO member) {
-		return dao.memberUpdate(member);
-	}
-
-	// 포인트 확인
-	public MemberVO memberPoint(String m_name, int point) {
-		return dao.selectByPoint(m_name, point);
-	}
-
-	// 관심매장 확인 및 수정
-	public MemberVO memberPlace(String m_name, String place) {
-		return dao.selectByPlace(m_name, place);
-
-	}
-
-	// 예매 및 취소내역 확인
-
-	// (리뷰)
-	
-	// 회원 탈퇴
-	public int Delete(String m_id) {
-		return dao.Delete(m_id);
 	}
 
 }
