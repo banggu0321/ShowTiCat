51a87bcc6e9a0349cc4570ab46fd0e414eb1578f
banggu0321 <99bang@naver.com>
2022-06-13 16:31:04 +0900

    add filter

--- src/main/java/com/kos/showticat/admin/ScheduleListServlet.java
+++ src/main/java/com/kos/showticat/admin/ScheduleListServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.kos.showticat.VO.MemberVO;
 
 /**
  * Servlet implementation class AdminScheduleList
@@ -15,10 +18,17 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/bang/schedule.do")
 public class ScheduleListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		ScheduleService service = new ScheduleService();
 		request.setAttribute("schedulelist", service.selectAllSchedule());
+		
+		HttpSession session = request.getSession(false);
+		//System.out.println((String)session.getAttributeNames());
+		//System.out.println(session.getId());
+		//MemberVO member = (MemberVO)session.getAttribute("member");
+		//System.out.println(member);
 		
 		RequestDispatcher rd;
 		rd = request.getRequestDispatcher("adminSchedule.jsp");
